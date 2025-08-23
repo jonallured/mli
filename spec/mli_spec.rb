d565907d00fb867373b619b376b541ed71b6416b
Jon Allured <jon@jonallured.com>
2025-08-23 08:51:04 -0500

    Initial commit
    Generated like so:
    
    $ bundler gem mli --exe --ci github --linter standard

---
+++ spec/mli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mli do
+  it "has a version number" do
+    expect(Mli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
