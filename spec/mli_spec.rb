92a54a592c9e71e56e96df707a8f0f9bcb8e6e96
Jon Allured <jon@jonallured.com>
2025-08-23 09:09:29 -0500

    Remove frozen string comment

--- spec/mli_spec.rb
+++ spec/mli_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe Mli do
   it "has a version number" do
     expect(Mli::VERSION).not_to be nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "mli"
 
 RSpec.configure do |config|
