8d02acac60cec672e75b51c1c62251dc63d6da98
Jon Allured <jon@jonallured.com>
2025-08-26 08:57:28 -0500

    Extract RootCommand class

---
+++ lib/mli/cli/root_command.rb
@@ -0,0 +1,20 @@
+require "thor"
+
+module Mli
+  module Cli
+    class RootCommand < Thor
+      desc "version", "Print the version number."
+      def version
+        say Mli::VERSION
+      end
+
+      def self.basename
+        "mli"
+      end
+
+      def self.exit_on_failure?
+        true
+      end
+    end
+  end
+end
---
+++ spec/lib/mli/cli/root_command_spec.rb
@@ -0,0 +1,29 @@
+require "mli/cli/root_command"
+
+RSpec.describe Mli::Cli::RootCommand do
+  describe "version" do
+    context "with no arguments" do
+      let(:argument_vector) { [] }
+
+      it "prints the help text" do
+        expected_output = File.read("spec/fixtures/cli/help.txt")
+
+        expect do
+          Mli::Cli::RootCommand.start(argument_vector)
+        end.to output(expected_output).to_stdout
+      end
+    end
+
+    context "with version command" do
+      let(:argument_vector) { %w[version] }
+
+      it "prints the version" do
+        expected_output = Mli::VERSION + "\n"
+
+        expect do
+          Mli::Cli::RootCommand.start(argument_vector)
+        end.to output(expected_output).to_stdout
+      end
+    end
+  end
+end
